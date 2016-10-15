d24fdd1a509a0f1168362c383f31d2c2f7a6e360
Dylan Paulus <dylanpaulus@Dylans-iMac.local>
2016-10-15 10:17:48 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+          {
+            test: /.jsx?$/,
+            loader: "babel-loader",
+            exclude: /node_modules/,
+            query: {
+              presets: ["es2015", "react"]
+            }
+          }
+        ]
+    },
+};
