28db1215f96259b497e5ef6892f33b82a08138fc
Dylan Paulus <dpaulus@Dylan-Pauluss-MacBook-Pro.local>
2016-12-14 22:20:15 -0800

    Add Scaffolding

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Header from "./Components/Header";
+
 const App = React.createClass({
 	render() {
 		return (
 			<section style={{ textAlign: "center" }}>
-				<h1>Hello!</h1>
+				<Header>Hello!</Header>
 			</section>
 		);
 	}
